12277f157d87578165ec3740a98310154912bc4a
Alec Rodriguez <alecrodriguez1247@gmail.com>
2025-01-27 19:14:33 -0600

    Create Main.hs

---
+++ app/Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Discord
+import DiscordOpts
+import Control.Monad (void)
+import Data.Text
+import Data.Text.IO
+import Commands
+
+
+getCatToken :: IO Text
+getCatToken = Data.Text.IO.readFile "apikeys/catToken"
+
+getDiscordToken :: IO Text
+getDiscordToken = Data.Text.IO.readFile "apikeys/discordToken"
+
+main :: IO ()
+main = do
+   cattoken <- getCatToken
+   bottoken <- getDiscordToken
+   result   <- runDiscord (myDiscordOpts bottoken cattoken [catimage,heart,joke])
+   Data.Text.IO.putStrLn result
