47556b0c7e725e8399de6243b9a623d154a70e30
Alec Rodriguez <alecrodriguez1247@gmail.com>
2025-02-01 10:24:11 -0600

    Update DiscordHandler.hs

--- src/DiscordHandler.hs
+++ src/DiscordHandler.hs
@@ -8,6 +8,7 @@ import Discord.Internal.Rest.Interactions
 import Discord.Internal.Rest.ApplicationCommands
 import Discord.Internal.Types.ApplicationCommands
 import Discord.Internal.Types.ApplicationInfo
+import Discord.Internal.Types.Prelude
 import Control.Monad.Reader
 import Data.Map as M
 
@@ -19,4 +20,10 @@ discordHandler commands =  do
         appid     = fullApplicationID fullapp
         guildIds  = keys $ cacheGuilds cache
 
+        -- oldCommandIds = [DiscordId $ Snowflake 1334904778983080017
+        --                ]
+
+    -- deleting old commands we don't need anymore
+
+    -- sequence_ ((\oldCommand -> restCall $ DeleteGlobalApplicationCommand appid oldCommand) <$> oldCommandIds)
     sequence_ ((\command -> restCall $ CreateGlobalApplicationCommand appid command) <$> commands)
