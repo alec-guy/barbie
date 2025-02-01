a7e80f2c83b88ca1c1ca2ead33bc0c3286984e6b
Alec Rodriguez <alecrodriguez1247@gmail.com>
2025-02-01 10:30:02 -0600

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -9,16 +9,28 @@ import Data.Text
 import Data.Text.IO
 import Commands
 
-
 getCatToken :: IO Text
 getCatToken = Data.Text.IO.readFile "apikeys/catToken"
 
 getDiscordToken :: IO Text
 getDiscordToken = Data.Text.IO.readFile "apikeys/discordToken"
 
+
+
+
 main :: IO ()
 main = do
+   let nasatok = "token is hidden from open source"
    cattoken <- getCatToken
    bottoken <- getDiscordToken
-   result   <- runDiscord (myDiscordOpts bottoken cattoken nasatok [catimage,joke, dogimage,foximage,minecraftR, heart,nasa,craftInfo])
-   Data.Text.IO.putStrLn result
+   result   <- runDiscord (myDiscordOpts bottoken cattoken nasatok [catimage
+                                                                   ,joke
+                                                                   , dogimage
+                                                                   ,foximage
+                                                                   ,minecraftR
+                                                                   , heart
+                                                                   ,nasa
+                                                                   ,craftInfo
+                                                                   ,help
+                                                                   ,pokemon
+                                                                   ])
